68229498dc59a0f2e2b2c4da4c627e6fb7a8b351
Mirek Pawelec <mirek.pawelec@wp.pl>
2020-03-04 00:23:04 +0100

    microservice started

---
+++ src/test/java/pl/pawelec/courseservice/CourseServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pawelec.courseservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
